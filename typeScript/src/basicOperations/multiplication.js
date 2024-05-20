4d95985a9c796ddb95ff6db354134c61d9169b35
Samuel Prado <samuelprado.99@hotmail.com>
2024-05-20 20:42:11 -0300

    feat :sparkles: The Multiplication Function Has Been Added To Scientific Calculator
    Adicionei o recurso de multiplicação

---
+++ typeScript/src/basicOperations/multiplication.js
@@ -0,0 +1,9 @@
+// Função para multiplicar dois números
+function multiplicar(a, b) {
+    return a * b;
+}
+// Exemplo de uso da função
+var num1 = 5;
+var num2 = 10;
+var resultado = multiplicar(num1, num2);
+console.log("O resultado da multiplica\u00E7\u00E3o de ".concat(num1, " e ").concat(num2, " \u00E9: ").concat(resultado));
---
+++ typeScript/src/basicOperations/multiplication.ts
@@ -0,0 +1,12 @@
+// Função para multiplicar dois números
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Exemplo de uso da função
+const num1: number = 5;
+const num2: number = 10;
+
+const resultado: number = multiplicar(num1, num2);
+
+console.log(`O resultado da multiplicação de ${num1} e ${num2} é: ${resultado}`);
