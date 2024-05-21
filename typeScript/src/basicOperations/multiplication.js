0404594d86ac34ff50e414663fdff6f3ef567e08
Samuel Prado <samuelprado.99@hotmail.com>
2024-05-21 19:55:53 -0300

    :bug: I've fixed the bugs
    Arrumei os codes

---
+++ typeScript/src/basicOperations/multiplication.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require("prompt-sync")(); // Inicializa o prompt-sync
+
+// Função principal para executar a calculadora de multiplicação
+function calculadora() {
+    var acVal = 1;
+    var continuar = true;
+    console.log("========== Multiplicação ==========");
+    console.log("Pressione 'F' para finalizar a operação");
+    while (continuar) {
+        console.log();
+        var valStr = prompt("Valor: ");
+        if (valStr.toLowerCase() === 'f') {
+            continuar = false;
+            break;
+        }
+        var val = parseInt(valStr, 10);
+        if (isNaN(val)) {
+            console.log("Entrada inválida, por favor insira um número.");
+            continue;
+        }
+        var acValStr = acVal;
+        acVal = acVal * val;
+        console.log("Resultado: ".concat(acValStr, " X ").concat(val, " = ").concat(acVal));
+    }
+    console.log("Opera\u00E7\u00E3o finalizada. Resultado final: ".concat(acVal));
+}
+
+// Chama a função principal para iniciar a calculadora
+calculadora();
