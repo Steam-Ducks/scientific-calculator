a6a6a8908799a48ce431e014663c31f59d9814ba
rafacruzz <rafaella.leticiaa@gmail.com>
2024-05-23 12:07:10 -0300

    ♻️ refactor: refactoring subtraction code

--- typeScript/src/basicOperations/subtraction.ts
+++ typeScript/src/basicOperations/subtraction.ts
@@ -1,19 +1,40 @@
-import promptSync from 'prompt-sync';
+import promptSync = require("prompt-sync");
 
 const prompt = promptSync();
 
-export function subtration(): void {
-    console.log()
-    const quantidade = parseInt(prompt("Quantos números deseja subtrair? "));
-    console.log()
-    let subtration = 0;
-    for (let i = 0; i < quantidade; i++) {
-        const num = parseFloat(prompt(`Digite o número ${i - 1}: `));
-        subtration -= num;
-    }
-    console.log()
-    console.log(`O resultado da subtração é: ${subtration}`);
-    console.log()
+export function subtraction(): void {
+  let value: number = 0;
+  let accumulatedValue: number = 0;
 
+  console.log();
+  const counter: number = parseInt(prompt("Quantos números deseja subtrair? "));
+
+  accumulatedValue = 0;
+  for (let i = 1; i <= counter; i++) {
+    if (i === counter && accumulatedValue === 0) {
+      console.log("Valor: ");
+      process.stdout.write(`${accumulatedValue} - `);
+      value = parseFloat(prompt(""));
+      accumulatedValue -= value;
+      break;
+    }
+
+    if (accumulatedValue === 0) {
+      console.log();
+      process.stdout.write("Valor: ");
+      accumulatedValue = parseFloat(prompt(""));
+    } else {
+      console.log();
+      console.log("Valor: ");
+      process.stdout.write(`${accumulatedValue} - `);
+      value = parseFloat(prompt(""));
+    }
+
+    accumulatedValue -= value;
+  }
+
+  console.log();
+  console.log(`O resultado da subtração é: ${accumulatedValue}`);
 }
 
+subtraction();
