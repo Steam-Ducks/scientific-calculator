8e8fffdadfc1cd6ad266ed7de98d0f932607d4e4
Lucas Acosta <lucas.heck@live.com>
2024-05-23 10:26:28 -0300

    ci: removing gitignore and adding default node_modules file

--- typeScript/build/basicOperations/multiplication.js
+++ typeScript/build/basicOperations/multiplication.js
@@ -4,28 +4,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
-function calculadora() {
-    const prompt = (0, prompt_sync_1.default)();
-    let acVal = 1;
-    let continuar = true;
-    console.log("========== Multiplicação ==========");
-    console.log("Pressione 'F' para finalizar a operação");
-    while (continuar) {
+const prompt = (0, prompt_sync_1.default)();
+function multiplicacao() {
+    let repete = 1;
+    while (repete === 1) {
+        console.log("========== Função Multiplicação ==========");
         console.log();
-        const valStr = prompt("Valor: ");
-        if (valStr.toLowerCase() === 'f') {
-            continuar = false;
-            break;
+        console.log("Insira os valores, e eles serão automaticamente multiplicados, ");
+        console.log();
+        console.log("para finalizar, pressione '=' ou multiplique por zero.");
+        console.log();
+        console.log("=============================================================");
+        console.log();
+        let valorAcumulado = 1.0;
+        while (true) {
+            let valorString = prompt(`Valor: ${valorAcumulado} * `);
+            if (valorString === '=' || valorString === '0') {
+                break;
+            }
+            let valor = parseFloat(valorString);
+            if (isNaN(valor)) {
+                console.log("Por favor, insira um número válido.");
+                continue;
+            }
+            valorAcumulado *= valor;
+            console.log(`Resultado: ${valorAcumulado}`);
+            console.log();
         }
-        const val = parseInt(valStr, 10);
-        if (isNaN(val)) {
-            console.log("Entrada inválida, por favor insira um número.");
-            continue;
+        console.log();
+        console.log(`O Resultado da multiplicação é: ${valorAcumulado}`);
+        console.log("=============================================================");
+        console.log();
+        repete = parseInt(prompt("Deseja calcular outro valor? \n1 <- Sim\n2 <- Não\nResposta: "));
+        console.log("=============================================================");
+        console.log("");
+        if (repete !== 1) {
+            console.log("Encerrando o programa...");
         }
-        const acValStr = acVal;
-        acVal = acVal * val;
-        console.log(`Resultado: ${acValStr} X ${val} = ${acVal}`);
     }
-    console.log(`Operação finalizada. Resultado final: ${acVal}`);
 }
-calculadora();
+multiplicacao();
---
+++ typeScript/build/specialOperations/quadraticFunction.js
@@ -0,0 +1,56 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.funcaoSegundoGrau = void 0;
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
+function funcaoSegundoGrau() {
+    let eq_a, eq_b, eq_c;
+    let delta, R1, R2, rdelta, precisao, raiz, nraiz;
+    precisao = 0.000001;
+    console.log("Cálculo de função do segundo grau!");
+    console.log();
+    console.log("Identifique os valores da função:");
+    console.log("f(x) = A.x^2 + B.x + C ");
+    console.log();
+    console.log("-----------------------------------");
+    eq_a = parseFloat(prompt("Entre com o valor de A: ") || '0');
+    eq_b = parseFloat(prompt("Entre com o valor de B: ") || '0');
+    eq_c = parseFloat(prompt("Entre com o valor de C: ") || '0');
+    console.log("----------------------------------");
+    console.clear();
+    console.log("----------------------------------");
+    delta = eq_b * eq_b - 4 * eq_a * eq_c;
+    console.log("O valor de delta é: ", delta);
+    console.log("----------------------------------");
+    if (delta < 0) {
+        console.log("Não existem raízes reais da função");
+        console.log();
+    }
+    else {
+        raiz = delta / 2;
+        rdelta = raiz;
+        while (raiz > precisao) {
+            nraiz = (raiz + delta / raiz) / 2;
+            if ((raiz - nraiz) < precisao) {
+                raiz = nraiz;
+                break;
+            }
+            raiz = nraiz;
+            rdelta = raiz;
+        }
+        rdelta = raiz;
+        R1 = ((-eq_b) + (rdelta)) / (2 * eq_a);
+        R2 = ((-eq_b) - (rdelta)) / (2 * eq_a);
+        console.log("----------------------------------");
+        console.log("A raiz x' é: ", R1.toFixed(2));
+        console.log();
+        console.log("A raiz x'' é: ", R2.toFixed(2));
+        console.log("----------------------------------");
+        console.log();
+    }
+}
+exports.funcaoSegundoGrau = funcaoSegundoGrau;
+funcaoSegundoGrau();
