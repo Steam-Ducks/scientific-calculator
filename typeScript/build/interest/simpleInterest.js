5ef20b157dfff41b9c6d925398934d19f370b665
Alexander Silva Lima <111926533+alexttz@users.noreply.github.com>
2024-05-25 11:32:30 -0300

    🔨 update simpleInterest.js
    Addition function simple Interest on code

--- typeScript/build/interest/simpleInterest.js
+++ typeScript/build/interest/simpleInterest.js
@@ -1,1 +1,20 @@
 "use strict";
+
+import * as prompt from 'prompt-sync';
+const input = prompt();
+
+console.log("========== JUROS SIMPLES ==========");
+console.log("");
+
+let valor = parseFloat(input("Digite o valor do empréstimo ou investimento: "));
+let taxa = parseFloat(input("Digite a porcentagem da taxa: "));
+let tempo = parseInt(input("Por último, informe o tempo decorrido em anos: "));
+console.log("");
+
+taxa = taxa / 100;
+
+let juros = valor * taxa * tempo;
+
+console.log("O valor de juros é de: ",juros);
+console.log("");
+console.log("====================================");
