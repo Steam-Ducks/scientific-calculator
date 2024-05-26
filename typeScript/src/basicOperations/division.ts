c7e2f67c6d652230bfe0d445086b985ab0789703
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2024-05-26 16:51:17 -0300

    ♻️ refactor: formatting float values and handling NaN errors

--- typeScript/src/basicOperations/division.ts
+++ typeScript/src/basicOperations/division.ts
@@ -1,13 +1,33 @@
 const prompt = require('prompt-sync')();
 
-
 export function divisao(): void {
+
     console.log()
     const quantidade = parseInt(prompt("Quantos números deseja dividir? "));
     console.log()
+
+    if (isNaN(quantidade) || quantidade <= 1) {
+        console.log("Por favor, insira um número válido de quantidades.");
+        return;
+      };
+
     let divisao = parseFloat(prompt(`Digite o 1º numero: `));
+
+    if (isNaN(divisao) || divisao <= 0)  {
+        console.log("Por favor, insira um número válido.");
+        return;
+      }
+
     for (let i = 1; i < quantidade; i++) {
         const numero = parseFloat(prompt(`Digite o ${i + 1}º numero: `));
+
+        let valor = parseFloat(numero);
+    if (isNaN(valor) || numero <= 0)  {
+      console.log("Por favor, insira um número válido.");
+      i--;
+      continue;
+    }
+
      divisao /= numero;
     }
     console.log()
