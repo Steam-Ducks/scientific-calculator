fd1bf6e18f037856382f956741fabcbbb5967ea2
Lucas Acosta <lucas.heck@live.com>
2024-06-09 10:25:34 -0300

    ♻️ refactor: input validation and text standardization

--- typeScript/build/basicOperations/addition.js
+++ typeScript/build/basicOperations/addition.js
@@ -10,15 +10,26 @@ function soma() {
     console.log("");
     console.log("========== SOMA ==========");
     console.log("");
-    const quantidade = parseInt(prompt("Quantos números deseja somar? "));
+    let quantidade = numeroValido("Quantos números deseja somar? ", 0);
     console.log();
     let soma = 0;
     for (let i = 0; i < quantidade; i++) {
-        const numero = parseFloat(prompt(`Digite o número ${i + 1}: `));
-        soma += numero;
+        soma += numeroValido(`Valor ${i + 1}: `);
     }
     console.log();
     console.log(`O resultado da soma é: ${soma}`);
     console.log();
 }
 exports.soma = soma;
+function numeroValido(texto, minimo = Number.MIN_SAFE_INTEGER) {
+    let numero;
+    while (true) {
+        numero = parseInt(prompt(texto));
+        if (!isNaN(numero) && numero > minimo) {
+            break;
+        }
+        console.log("Por favor, insira um número válido.");
+        console.log();
+    }
+    return numero;
+}
--- typeScript/src/basicOperations/addition.ts
+++ typeScript/src/basicOperations/addition.ts
@@ -6,15 +6,28 @@ export function soma(): void {
     console.log("");
     console.log("========== SOMA ==========");
     console.log("");
-    const quantidade = parseInt(prompt("Quantos números deseja somar? "));
+    let quantidade: number = numeroValido("Quantos números deseja somar? ", 0);
     console.log()
     let soma = 0;
-    for (let i = 0; i < quantidade; i++) {
-        const numero = parseFloat(prompt(`Digite o número ${i + 1}: `));
-        soma += numero;
+    for (let i = 0; i < quantidade; i++) {  
+        soma += numeroValido(`Valor ${i + 1}: `);
     }
     console.log()
     console.log(`O resultado da soma é: ${soma}`);
     console.log()
 
+}
+function numeroValido(texto: string, minimo: number = Number.MIN_SAFE_INTEGER): number {
+    let numero: number;
+    while (true) {
+        numero = parseInt(prompt(texto));
+        if (!isNaN(numero) && numero > minimo)
+        {
+            break;
+        }
+        console.log("Por favor, insira um número válido.")
+        console.log();
+    }
+
+    return numero;
 }
