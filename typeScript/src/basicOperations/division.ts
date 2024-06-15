a243aea2a7ca9513b5027e8fcd1f42bf35a68b35
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2024-06-15 14:04:38 -0300

    💄feat: fixing input validation on division function

--- typeScript/src/basicOperations/division.ts
+++ typeScript/src/basicOperations/division.ts
@@ -6,32 +6,39 @@ export function divisao(): void {
     console.log("========== DIVISÃO ==========");
     console.log("");
 
-//==================================================================================
-// declara a variavel de quantidade
+ //==================================================================================
+ // declara a variavel de quantidade
     let quantidade = 0;
     do {
       quantidade = parseInt(prompt("Quantos números deseja dividir? "));
-// checa se o nuero é maior que 1
+ // checa se o nuero é maior que 1
       if (isNaN(quantidade) || quantidade <= 1) {
           console.log("Por favor, insira um número válido.");
       console.log("");
       }
-//aqui está a regra de ser maior ou igual a 1. Não da para dividir menos de dois numeros
+ //aqui está a regra de ser maior ou igual a 1. Não da para dividir menos de dois numeros
   } while (isNaN(quantidade) || quantidade <= 1);
   
-//==================================================================================
-//Recebe o numero dividendo
+ //==================================================================================
+ //Recebe o numero dividendo
       let divisao = 0 
-      divisao = parseFloat(prompt(`Valor: `));  
+      do {
+      divisao = parseFloat(prompt(`Valor: `)); 
+      if (isNaN(divisao)) {
+        console.log("Por favor, insira um número válido.");
+    console.log("");
+    }
+ //aqui está a regra de ser um numero
+        } while (isNaN(divisao));
 
-//==================================================================================
+ //==================================================================================
 
-// Recebe os dividendos
+ // Recebe os dividendos
     for (let i = 1; i < quantidade; i++) {
 
         const numero = parseFloat(prompt(`Valor: `));
 
-// Verifica se não está sendo dividido por 0
+ // Verifica se não está sendo dividido por 0
     if (isNaN(numero) || numero <= 0)  {
       console.log("Não é posspivel dividir por 0");
       console.log("Por favor, insira um número válido.");
@@ -40,12 +47,12 @@ export function divisao(): void {
       continue;
     }
 
-// Realiza a operação
+ // Realiza a operação
      divisao /= numero;
 
-//fecha o loop
+ //fecha o loop
     }
-//Exibe o resultado
+ //Exibe o resultado
     console.log()
     console.log(`O resultado da divisao é: ${divisao}`);
     console.log()
