c33a557ef12826bb439b95c54b3f1bac5de7678d
TheodaRosa <theo.smidt@fatec.sp.gov.br>
2024-06-16 13:48:52 -0300

    :recycle: refactor: Compiling Division
    Compiliing the Division code

--- typeScript/build/basicOperations/division.js
+++ typeScript/build/basicOperations/division.js
@@ -1,29 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.divisao = void 0;
-const prompt = require('prompt-sync')();
+var prompt = require('prompt-sync')();
 function divisao() {
     console.log("");
     console.log("========== DIVISÃO ==========");
     console.log("");
-    let quantidade = 0;
+    //==================================================================================
+    // declara a variavel de quantidade
+    var quantidade = 0;
     do {
         quantidade = parseInt(prompt("Quantos números deseja dividir? "));
+        // checa se o nuero é maior que 1
         if (isNaN(quantidade) || quantidade <= 1) {
             console.log("Por favor, insira um número válido.");
             console.log("");
         }
+        //aqui está a regra de ser maior ou igual a 1. Não da para dividir menos de dois numeros
     } while (isNaN(quantidade) || quantidade <= 1);
-    let divisao = 0;
+    //==================================================================================
+    //Recebe o numero dividendo
+    var divisao = 0;
     do {
-        divisao = parseFloat(prompt(`Valor: `));
+        divisao = parseFloat(prompt("Valor: "));
         if (isNaN(divisao)) {
             console.log("Por favor, insira um número válido.");
             console.log("");
         }
+        //aqui está a regra de ser um numero
     } while (isNaN(divisao));
-    for (let i = 1; i < quantidade; i++) {
-        const numero = parseFloat(prompt(`Valor: `));
+    //==================================================================================
+    // Recebe os dividendos
+    for (var i = 1; i < quantidade; i++) {
+        var numero = parseFloat(prompt("Valor: "));
+        // Verifica se não está sendo dividido por 0
         if (isNaN(numero) || numero <= 0) {
             console.log("Não é posspivel dividir por 0");
             console.log("Por favor, insira um número válido.");
@@ -31,10 +41,13 @@ function divisao() {
             i--;
             continue;
         }
+        // Realiza a operação
         divisao /= numero;
+        //fecha o loop
     }
+    //Exibe o resultado
     console.log();
-    console.log(`O resultado da divisao é: ${divisao}`);
+    console.log("O resultado da divisao \u00E9: ".concat(divisao));
     console.log();
 }
 exports.divisao = divisao;
