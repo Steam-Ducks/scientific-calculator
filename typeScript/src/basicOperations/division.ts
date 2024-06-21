3776dacf2a2908e3b5f76cea4afa75dcb43d34d6
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2024-06-21 09:36:05 -0300

    ♻️ refactor: adding data types to variables

--- typeScript/src/basicOperations/division.ts
+++ typeScript/src/basicOperations/division.ts
@@ -1,5 +1,5 @@
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
+import promptSync from 'prompt-sync'; // Importa o módulo prompt-sync
+const prompt = promptSync(); // Cria uma instância de prompt-sync
 
 export function divisao(): void {
     console.log("");
@@ -10,16 +10,16 @@ export function divisao(): void {
 
     //==================================================================================
     // Declara as variáveis com tipos
-    let num: string = "";
-    let validInput: boolean;
-    let quantidade: number = 0;
+    let num: string = ""; // Variável para armazenar a entrada do usuário
+    let validInput: boolean; // Variável para verificar se a entrada é válida
+    let quantidade: number = 0; // Variável para armazenar a quantidade de números a dividir
 
     do {
-        num = prompt("Quantos números deseja dividir? ");
+        num = prompt("Quantos números deseja dividir? "); // Solicita a quantidade de números ao usuário
         
         validInput = /^\d+$/.test(num); // Verifica se a entrada contém apenas dígitos
 
-        quantidade = parseInt(num);
+        quantidade = parseInt(num); // Converte a entrada para um número
 
         // Checa se o número é maior que 1 e se a entrada é válida (somente números)
         if (!validInput || isNaN(quantidade) || quantidade <= 1) {
@@ -30,14 +30,13 @@ export function divisao(): void {
 
     //==================================================================================
     // Recebe o número dividendo
-    let dividendo: number = 0;
+    let dividendo: number = 0; // Variável para armazenar o dividendo
 
     do {
-        num = prompt(`Valor: `);
-        validInput = /^[0-9]+(\.[0-9]+)?$/.test(num); // Verifica se a entrada contém apenas dígitos
-
-        dividendo = parseFloat(num);
-        dividendo = parseFloat(dividendo.toFixed(2));
+        num = prompt(`Valor: `); // Solicita o número do dividendo
+        validInput = /^-?\d*\.?\d+$/.test(num); // Verifica se a entrada é um número
+        dividendo = parseFloat(num); // Converte a entrada para um número de ponto flutuante
+        dividendo = parseFloat(dividendo.toFixed(2)); // Fixa a precisão do número
 
         // Checa se o número é válido (somente números)
         if (!validInput || isNaN(dividendo)) {
@@ -48,38 +47,33 @@ export function divisao(): void {
 
     //==================================================================================
     // Recebe os divisores
-    let divisor: number = 0;
+    let divisor: number = 0; // Variável para armazenar os divisores
 
     for (let i = 1; i < quantidade;) {
         do {
             i++;
             console.log();
             console.log('Valor:');
-            num = prompt(`${dividendo} / `);
-
-            validInput = /^[0-9]+(\.[0-9]+)?$/.test(num); // Verifica se a entrada contém apenas dígitos
-
-            divisor = parseFloat(num);
-            divisor = parseFloat(divisor.toFixed(2));
-
-            // Checa se o número é válido e se não está sendo dividido por 0
-            if (!validInput || isNaN(divisor) || divisor <= 0) {
+            num = prompt(`${dividendo} / `); // Solicita o divisor
+            validInput = /^-?\d*\.?\d+$/.test(num); // Verifica se a entrada é um número
+            divisor = parseFloat(num); // Converte a entrada para um número de ponto flutuante
+            divisor = parseFloat(divisor.toFixed(2)); // Fixa a precisão do número
+            if (!validInput || isNaN(divisor) || divisor == 0) {
                 console.log("Por favor, insira um número válido.");
                 console.log("");
                 i--;
             }
-        } while (!validInput || isNaN(divisor) || divisor <= 0);
+        } while (!validInput || isNaN(divisor) || divisor == 0);
 
-        // Realiza a operação
+        // Realiza a operação de divisão
         dividendo /= divisor;
-        dividendo = parseFloat(dividendo.toFixed(2));
+        dividendo = parseFloat(dividendo.toFixed(2)); // Fixa a precisão do resultado da divisão
     }
 
-    const resul: number = parseFloat(dividendo.toFixed(2));
+    const resul: number = parseFloat(dividendo.toFixed(2)); // Fixa a precisão final do resultado
 
-    // Exibe o resultado
+    // Exibe o resultado da divisão
     console.log();
-    console.log(`O resultado da divisao é: ${resul}`);
+    console.log(`O resultado da divisão é: ${resul}`);
     console.log("═════════════════════════════════════");
-
 }
